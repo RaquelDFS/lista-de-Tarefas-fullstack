eaed973d2dd170a7eb98557a966e2f822c577ba2
Raquel Fonseca <97365473+RaquelDFS@users.noreply.github.com>
2024-07-12 18:40:57 -0300

    consumindo a api

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mysql = require('mysql');
 
-const port = process.env.PORT || 3000;
 
 
 const connection = mysql.createConnection({
@@ -13,14 +12,30 @@ const connection = mysql.createConnection({
 
 
 const app = new express();
-app.listen(port, () => {
+app.listen(3000, () => {
     console.log('Servidor iniciado')
 })
 
+
+
+
+
+
+//Rotas--------------------------------
 app.get("/", (req, res) => {
     connection.query("SELECT COUNT(*) users FROM users", (err, results) => {
         if (err) {
             res.send(err.message);
         } res.send(results);
+    })
+})
+
+app.get("/user/:id", (req, res) => {
+    let id = req.params.id;
+    //Para completo: SELECT * FROM users WHERE id = ?
+    connection.query("SELECT id, username, created_at, updated_at FROM users WHERE id = ?", [req.params.id], (err, results) => {
+        if (err) {
+            res.send(err.message);
+        } res.json(results);
     })
 })
